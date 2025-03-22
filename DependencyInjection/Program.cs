62a33b8066c2275bd86ec4a49af5b4a15c3a8090
dinesh-asaware <dinesh.asaware30@gmail.com>
2025-03-22 19:26:18 +0530

    Add files via upload
    Design paterns examples

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public interface IAccount
+    {
+        void PrintDetails();
+
+    }
+
+    public class CurrentAccount : IAccount
+    {
+        public void PrintDetails() 
+        {
+            Console.WriteLine("details of Current account ");
+        }
+    }
+
+    public class SavingAccount : IAccount
+    {
+        public void PrintDetails()
+        {
+            Console.WriteLine("details of Saving account ");
+        }
+    }
+
+    public class Account
+    {
+        public void AccountDetails(IAccount account)
+        {
+            account.PrintDetails();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account ca = new Account();
+            ca.AccountDetails(new CurrentAccount());
+
+            Account sa = new Account();
+            sa.AccountDetails(new SavingAccount());
+
+            Console.ReadLine();
+        }
+    }
+}
