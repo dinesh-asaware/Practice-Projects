bd9816f73b4e9d8bf3526c4d8cb3ee2a5d963afb
dinesh-asaware <dinesh.asaware30@gmail.com>
2025-03-22 19:28:00 +0530

    Add files via upload

---
+++ AdapterDesignPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPattern
+{
+
+    public interface ITarget
+    {
+        void Request();
+    }
+
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("This is method of SpecificRequest");
+        }
+    }
+
+    public class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public void Request()
+        {
+            _adaptee.SpecificRequest();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            target.Request();
+
+            Console.ReadLine();
+        }
+    }
+}
