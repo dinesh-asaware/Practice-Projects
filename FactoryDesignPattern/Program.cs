eca6ee970f16c5103b52450228ea7531c6743d46
dinesh-asaware <dinesh.asaware30@gmail.com>
2025-03-22 19:30:12 +0530

    Add files via upload

---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+
+    public interface Ivehicle
+    {
+        void Drive();
+    }
+
+    public class Car: Ivehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving car.....");
+            
+        }
+    }
+
+    public class Bike : Ivehicle
+    { 
+        public void Drive()
+        {
+            Console.WriteLine("Ridding bike....");
+        }
+    }
+
+    public class vehicleFactory
+    {
+        public static Ivehicle GetIvehicle(string vehiclename)
+        {
+                Ivehicle obj = null;
+            
+                if (vehiclename.ToLower().Equals("car"))
+                {
+                    obj = new Car();
+
+                }
+
+                else if (vehiclename.ToLower().Equals("bike"))
+                {
+                    obj = new Bike();
+                }
+                
+                 return obj ;
+               
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ivehicle vehicle1 = vehicleFactory.GetIvehicle("Car");
+            vehicle1.Drive();
+
+            Ivehicle vehicle2 = vehicleFactory.GetIvehicle("Bike");
+            vehicle2.Drive();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
